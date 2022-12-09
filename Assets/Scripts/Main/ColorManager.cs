c5f8ebf51a0dcf8a8c5cd3c8dcc2d3b7124e80f3
A20010otawa <san-a20010@sist.ac.jp>
2022-12-09 12:33:12 +0900

    1209_4

--- Assets/Scripts/Main/ColorManager.cs
+++ Assets/Scripts/Main/ColorManager.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Material PlayerMate;
+    public Slider H, S, V;
+    public Image Simage;
+    private float H_flort, S_flort, V_flort;
     // Update is called once per frame
+
+    private void Awake()
+    {
+        float r = (Convert.ToInt32(PlayerData.SaveData["color"], 16) >> 16) & 0xff;
+        float g = (Convert.ToInt32(PlayerData.SaveData["color"], 16) >> 8) & 0xff;
+        float b = Convert.ToInt32(PlayerData.SaveData["color"], 16) & 0xff;
+        //PlayerMate.color =new Color(r / 255, g / 255, b / 255);
+        Color.RGBToHSV(new Color(r / 255, g / 255, b / 255), out H_flort, out S_flort, out V_flort);
+
+        H.value = H_flort;
+        S.value = S_flort;
+        V.value = V_flort;
+    }
     void Update()
     {
-        
+        Simage.color = Color.HSVToRGB(H.value, S.value, V.value);
+        PlayerMate.color = Color.HSVToRGB(H.value, S.value, V.value);
+    }
+    public void SetEnd()
+    {
+        int r = (int)(PlayerMate.color.r * 255);
+        int g = (int)(PlayerMate.color.g * 255);
+        int b = (int)(PlayerMate.color.b * 255);
+        string R = r.ToString("x");
+        string G = g.ToString("x");
+        string B = b.ToString("x");
+        string RGB = R + G + B;
+        PlayerData.SaveData["color"] = RGB;
     }
 }
